ebbfd5a83b65ac819641c67fd5bd67085fc5c647
Nurdoğan Bahadır <nurdoganbahadirr@gmail.com>
2024-09-07 18:24:55 +0300

    my-library addBook sayfası tamamladnı

--- my-library/src/components/addBook.jsx
+++ my-library/src/components/addBook.jsx
@@ -1,9 +1,73 @@
-import React from 'react'
+import React from "react";
 
 const AddBook = () => {
   return (
-    <div>addBook</div>
-  )
-}
+    <div className="container my-5">
+      <h1 className="text-center">My Library</h1>
+      <div className="row">
+        <div className="col-sm-12">
+          <form>
+            <div className="mb-3">
+              <label htmlFor="bookName" className="form-label">
+                Book Name
+              </label>
+              <input
+                type="text"
+                placeholder="Book Name"
+                className="form-control"
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="bookName" className="form-label">
+                Writer
+              </label>
+              <input
+                type="text"
+                placeholder="Writer"
+                className="form-control"
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="bookName" className="form-label">
+                ISBN
+              </label>
+              <input type="text" placeholder="ISBN" className="form-control" />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="bookName" className="form-label">
+                Kapak Resmi
+              </label>
+              <input
+                type="text"
+                placeholder="Kapak Resmi"
+                className="form-control"
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="bookName" className="form-label">
+                Tür
+              </label>
+              <input type="text" placeholder="Tür" className="form-control" />
+              <p>Birden fazla seçenek varsa slash ile ayırabilirsiniz.</p>
+            </div>
+            <div className="mb-3">
+              <label htmlFor="bookName" className="form-label">
+                Yayınlanm Tarihi
+              </label>
+              <input
+                type="text"
+                placeholder="Yayınlanma Tarihi"
+                className="form-control"
+              />
+            </div>
+            <button type="submit" className="btn btn-primary">
+              Gönder
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default AddBook
+export default AddBook;
--- my-library/src/pages/HomePage.jsx
+++ my-library/src/pages/HomePage.jsx
@@ -12,6 +12,7 @@ const HomePage = () => {
     const response = await axios.get(url);
     setBooks(response.data);
   };
+
   useEffect(() => {
     getBooks();
   }, []);

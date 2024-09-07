55968382ebab04106cefa5a235fbc2add76c7654
Nurdoğan Bahadır <nurdoganbahadirr@gmail.com>
2024-09-07 17:58:18 +0300

    my-library axios added

--- my-library/src/pages/HomePage.jsx
+++ my-library/src/pages/HomePage.jsx
@@ -1,8 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import AddBook from "../components/AddBook";
 import BookList from "../components/BookList";
+import axios from "axios";
 
 const HomePage = () => {
+  const [books, setBooks] = useState([]);
+
+  const url = "https://tutorial-api.fullstack.clarusway.com/tutorials/";
+
+  const getBooks = async () => {
+    const response = await axios.get(url);
+    setBooks(response.data);
+  };
+  useEffect(() => {
+    getBooks();
+  }, []);
+
   return (
     <>
       <AddBook />

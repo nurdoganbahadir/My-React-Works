e5f342f4b2887fb94f04b23cf86a331896e9f3f1
Nurdoğan Bahadır <nurdoganbahadirr@gmail.com>
2024-09-09 16:45:57 +0300

    delete işlemi homepage'ye eklendi

--- my-library/src/pages/HomePage.jsx
+++ my-library/src/pages/HomePage.jsx
@@ -8,16 +8,22 @@ const HomePage = () => {
 
   const url = "https://clarus-library-api.vercel.app/books";
 
+  //!GET-READ
   const getBooks = async () => {
     const response = await axios.get(url);
     setBooks(response.data);
   };
-
+  //! POST
   const postBooks = async (newData) => {
     await axios.post(url, newData);
     getBooks();
   };
-
+  //! DELETE
+  const deleteBooks = async (id) => {
+    await axios.delete(`${url}${id}/`);
+    getBooks();
+  };
+  
   useEffect(() => {
     getBooks();
   }, []);
